21a71e0f28433fedb9fff9d0ddd438104d0d5989
Matthew Haas <haas@corning-cc.edu>
2026-03-02 11:18:46 -0500

    usage.c: incorrect description for . fixing.

--- usage.c
+++ usage.c
@@ -7,7 +7,7 @@ void  usage (int8_t *program)
     fprintf (stdout, "Mandatory arguments to long options are mandatory ");
     fprintf (stdout, "for short options too.\n\n");
     fprintf (stdout, " -B, --biosfile=FILE      load this BIOS V32 file as BIOS\n");
-    fprintf (stdout, " -C, --command-file=FILE  load this BIOS V32 file as BIOS\n");
+    fprintf (stdout, " -C, --command-file=FILE  load this file with sim commands\n");
 //    fprintf (stdout, " -c, --colors           enable colorized output\n");
     fprintf (stdout, " -i, --index-math         output index math after decode\n");
     fprintf (stdout, " -r, --run                do not enable single-step mode\n");
