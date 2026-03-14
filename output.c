596e4c43c8628dc6128f62b0bac331115eda41d7
Matthew Haas <haas@corning-cc.edu>
2026-03-14 15:19:36 -0400

    v32sim changes:
      * output.c: ripped out all the fancy output coloring
    
    De-complicated the output_reg() and output_mem() functions. Just the
    single cyan color (plus any light yellow labels). Spacing calibration
    works once again. Invalid addresses work (as in are reported).

--- output.c
+++ output.c
@@ -2,46 +2,28 @@
 
 void  output_reg (uint8_t  id, uint8_t  fmt, uint8_t  flag, uint8_t *label)
 {
-    int32_t   check          = TRUE;
-    int32_t   index          = 0;
-    uint32_t  data           = 0;
+    int32_t   check               = TRUE;
+    int32_t   index               = 0;
+    uint32_t  data                = 0;
     uint8_t   entry[64];
-    uint8_t   escape         = 0;
 
-    if (flag                == TRUE)
+    if (flag                     == TRUE)
     {
-        if (colorflag       == TRUE)
-        {
-            sprintf (entry, "\e[m\e[0;31m[\e[m\e[0;34m%s(0x%.8X)\e[m\e[0;31m]",
-                    REGNAME(id), REG(id));
-            escape           = 24;
-        }
-        else
-        {
-            sprintf (entry, "[%s(0x%.8X)]", REGNAME(id), REG(id));
-        }
-        check                = memory_chk (REG(id), TRUE);
+        sprintf (entry, "[%s(0x%.8X)]", REGNAME(id), REG(id));
+        check                     = memory_chk (REG(id), TRUE);
     }
     else
     {
-        if (colorflag       == TRUE)
-        {
-            sprintf (entry, "\e[m\e[0;34m%s", REGNAME(id));
-            escape           = 12;
-        }
-        else
-        {
-            sprintf (entry, "%s", REGNAME(id));
-        }
+        sprintf (entry, "%s", REGNAME(id));
     }
 
     ////////////////////////////////////////////////////////////////////////////////////
     //
     // Recalibrate spacing if needed (ie encounter a wider displaylist member)
     //
-    if (dpoint -> space     <  (strlen (entry) - escape))
+    if (dpoint -> space          <  strlen (entry))
     {
-        dpoint -> space      = strlen (entry) - escape;
+        dpoint -> space           = strlen (entry);
     }
 
     ////////////////////////////////////////////////////////////////////////////////////
@@ -50,247 +32,123 @@ void  output_reg (uint8_t  id, uint8_t  fmt, uint8_t  flag, uint8_t *label)
     //
     fprintf (stdout, "%*s", dpoint -> space, entry);
 
-    if (check               == FALSE)
+    if (check                    == FALSE)
     {
-        if (colorflag       == TRUE)
-        {
-            fprintf (stdout, "\e[m: \e[1;37m<\e[m\e[1;31minvalid address\e[m\e[1;37m>\e[m");
-        }
-        else
-        {
-            fprintf (stdout, ": <invalid address>");
-        }
+        fprintf (stdout, ": <invalid address>");
     }
     else
     {
         switch (fmt)
         {
             case FORMAT_DEFAULT:
-                if (flag    == TRUE)
+                if (flag         == TRUE)
                 {
-                    if (colorflag == TRUE)
-                    {
-                        fprintf (stdout, "\e[m: \e[1;36m0x%.8X",  IMEMGET(REG(id)));
-                    }
-                    else
-                    {
-                        fprintf (stdout, ": 0x%.8X",              IMEMGET(REG(id)));
-                    }
+                    fprintf (stdout, ": 0x%.8X",   IMEMGET(REG(id)));
                 }
                 else
                 {
-                    if (colorflag == TRUE)
-                    {
-                        fprintf (stdout, "\e[m: \e[1;36m0x%.8X",  REG(id));
-                    }
-                    else
-                    {
-                        fprintf (stdout, ": 0x%.8X",              REG(id));
-                    }
+                    fprintf (stdout, ": 0x%.8X",   REG(id));
                 }
                 break;
 
             case FORMAT_HEX:
-                if (flag    == TRUE)
+                if (flag         == TRUE)
                 {
-                    if (colorflag == TRUE)
-                    {
-                        fprintf (stdout, "/X\e[m: \e[1;36m0x%.8X",  IMEMGET(REG(id)));
-                    }
-                    else
-                    {
-                        fprintf (stdout, "/X: 0x%.8X",              IMEMGET(REG(id)));
-                    }
+                    fprintf (stdout, "/X: 0x%.8X", IMEMGET(REG(id)));
                 }
                 else
                 {
-                    if (colorflag == TRUE)
-                    {
-                        fprintf (stdout, "/X\e[m: \e[1;36m0x%.8X",  REG(id));
-                    }
-                    else
-                    {
-                        fprintf (stdout, "/X: 0x%.8X",              REG(id));
-                    }
+                    fprintf (stdout, "/X: 0x%.8X", REG(id));
                 }
                 break;
 
             case FORMAT_LOWERHEX:
-                if (flag    == TRUE)
+                if (flag         == TRUE)
                 {
-                    if (colorflag == TRUE)
-                    {
-                        fprintf (stdout, "/x\e[m: \e[1;36m0x%.8X",  IMEMGET(REG(id)));
-                    }
-                    else
-                    {
-                        fprintf (stdout, "/x: 0x%.8X",              IMEMGET(REG(id)));
-                    }
+                    fprintf (stdout, "/x: 0x%.8X", IMEMGET(REG(id)));
                 }
                 else
                 {
-                    if (colorflag == TRUE)
-                    {
-                        fprintf (stdout, "/x\e[m: \e[1;36m0x%.8x",  REG(id));
-                    }
-                    else
-                    {
-                        fprintf (stdout, "/x: 0x%.8x",              REG(id));
-                    }
+                    fprintf (stdout, "/x: 0x%.8x", REG(id));
                 }
                 break;
 
             case FORMAT_UNSIGNED:
-                if (flag    == TRUE)
+                if (flag         == TRUE)
                 {
-                    if (colorflag == TRUE)
-                    {
-                        fprintf (stdout, "/u\e[m: \e[1;36m%u",      IMEMGET(REG(id)));
-                    }
-                    else
-                    {
-                        fprintf (stdout, "/u: %u",                  IMEMGET(REG(id)));
-                    }
+                    fprintf (stdout, "/u: %u",     IMEMGET(REG(id)));
                 }
                 else
                 {
-                    if (colorflag == TRUE)
-                    {
-                        fprintf (stdout, "/u\e[m: \e[1;36m%u",      REG(id));
-                    }
-                    else
-                    {
-                        fprintf (stdout, "/u: %u",                  REG(id));
-                    }
+                    fprintf (stdout, "/u: %u",     REG(id));
                 }
                 break;
 
             case FORMAT_OCTAL:
-                if (flag    == TRUE)
+                if (flag         == TRUE)
                 {
-                    if (colorflag == TRUE)
-                    {
-                        fprintf (stdout, "/o\e[m: \e[1;36m0%o",     IMEMGET(REG(id)));
-                    }
-                    else
-                    {
-                        fprintf (stdout, "/o: 0%o",                 IMEMGET(REG(id)));
-                    }
+                    fprintf (stdout, "/o: 0%o",    IMEMGET(REG(id)));
                 }
                 else
                 {
-                    if (colorflag == TRUE)
-                    {
-                        fprintf (stdout, "/o\e[m: \e[1;36m0%o",     REG(id));
-                    }
-                    else
-                    {
-                        fprintf (stdout, "/o: 0%o",                 REG(id));
-                    }
+                    fprintf (stdout, "/o: 0%o",    REG(id));
                 }
                 break;
 
             case FORMAT_FLOAT:
-                if (flag    == TRUE)
+                if (flag         == TRUE)
                 {
-                    if (colorflag == TRUE)
-                    {
-                        fprintf (stdout, "/f\e[m: \e[1;36m%.2f",    FMEMGET(REG(id)));
-                    }
-                    else
-                    {
-                        fprintf (stdout, "/f: %.2f",                FMEMGET(REG(id)));
-                    }
+                    fprintf (stdout, "/f: %.2f",   FMEMGET(REG(id)));
                 }
                 else
                 {
-                    if (colorflag == TRUE)
-                    {
-                        fprintf (stdout, "/f\e[m: \e[1;36m%.2f",    FREG(id));
-                    }
-                    else
-                    {
-                        fprintf (stdout, "/f: %.2f",                FREG(id));
-                    }
+                    fprintf (stdout, "/f: %.2f",   FREG(id));
                 }
                 break;
 
             case FORMAT_SIGNED:
-                if (flag    == TRUE)
+                if (flag         == TRUE)
                 {
-                    if (colorflag == TRUE)
-                    {
-                        fprintf (stdout, "/d\e[m: \e[1;36m%d",      IMEMGET(REG(id)));
-                    }
-                    else
-                    {
-                        fprintf (stdout, "/d: %d",                  IMEMGET(REG(id)));
-                    }
+                    fprintf (stdout, "/d: %d",     IMEMGET(REG(id)));
                 }
                 else
                 {
-                    if (colorflag == TRUE)
-                    {
-                        fprintf (stdout, "/d\e[m: \e[1;36m%d",      REG(id));
-                    }
-                    else
-                    {
-                        fprintf (stdout, "/d: %d",                  REG(id));
-                    }
+                    fprintf (stdout, "/d: %d",     REG(id));
                 }
                 break;
 
             case FORMAT_BINARY:
-                if (flag          == TRUE)
+                fprintf (stdout, "/b: ");
+                if (flag         == TRUE)
                 {
-                    data           = IMEMGET(REG(id));
-                    if (colorflag == TRUE)
-                    {
-                        fprintf (stdout, "/b\e[m: \e[1;36m");
-                    }
-                    else
-                    {
-                        fprintf (stdout, "/b: ");
-                    }
+                    data          = IMEMGET(REG(id));
                 }
                 else
                 {
-                    data           = REG(id);
-                    if (colorflag == TRUE)
-                    {
-                        fprintf (stdout, "/b\e[m: \e[1;36m");
-                    }
-                    else
-                    {
-                        fprintf (stdout, "/b: ");
-                    }
+                    data          = REG(id);
                 }
 
-                for (index         = 31;
-                     index        >= 0;
-                     index         = index - 1)
+                for (index        = 31;
+                     index       >= 0;
+                     index        = index - 1)
                 {
-                    entry[index]   = (data % 2) + 0x30;
-                    data           = data / 2;
+                    entry[index]  = (data % 2) + 0x30;
+                    data          = data / 2;
                 }
-                entry[32]          = '\0';
+                entry[32]         = '\0';
                 fprintf (stdout, "%s", entry);
                 break;
         }
-        if (colorflag       == TRUE)
-        {
-            fprintf (stdout, "\e[m");
-        }
     }
 
-    if (label           != NULL)
+    if (label                    != NULL)
     {
-        if (colorflag   == TRUE)
+        if (colorflag            == TRUE)
         {
             fprintf (stdout, "\e[0;33m");
         }
         fprintf (stdout, " \"%s\"", label);
-        if (colorflag   == TRUE)
+        if (colorflag            == TRUE)
         {
             fprintf (stdout, "\e[m");
         }
@@ -304,138 +162,97 @@ void  output_mem (uint32_t  value, uint8_t  fmt,  uint8_t  flag, uint8_t *label)
     int32_t   index          = 0;
     uint32_t  data           = 0;
     uint8_t   entry[64];
-    uint8_t   escape         = 0;
 
     if (flag                == TRUE)
     {
         data                 = ISYSMEMGET(value);
-        if (colorflag       == TRUE)
-        {
-            sprintf (entry, "\e[m\e[0;31m[\e[m\e[0;34m0x%.8X(0x%.8X)\e[0;31m]\e[m",
-                    value, data);
-            escape           = 17;
-        }
-        else
-        {
-            sprintf (entry, "[0x%.8X(0x%.8X)]", value, data);
-        }
+        sprintf (entry, "[0x%.8X(0x%.8X)]", value, data);
         check                = memory_chk (value, TRUE) & memory_chk (data, TRUE);
     }
     else
     {
-        if (colorflag       == TRUE)
-        {
-            sprintf (entry, "\e[m\e[0;34m0x%.8X\e[m", value);
-            escape           = 8;
-        }
-        else
-        {
-            sprintf (entry, "0x%.8X", value);
-        }
+        sprintf (entry, "0x%.8X", value);
         check                = memory_chk (value, TRUE);
     }
 
-    if (dpoint -> space     <  (strlen (entry) - escape))
+    if (dpoint -> space     <  strlen (entry))
     {
-        dpoint -> space      = strlen (entry) - escape;
+        dpoint -> space      = strlen (entry);
     }
 
     fprintf (stdout, "%*s: ", dpoint -> space, entry);
 
     if (check               == FALSE)
     {
-        if (colorflag       == TRUE)
-        {
-            fprintf (stdout, "\e[1;37m<\e[m\e[1;31minvalid address\e[m\e[1;37m>\e[m");
-        }
-        else
-        {
-            fprintf (stdout, "<invalid address>");
-        }
+        fprintf (stdout, "<invalid address>");
     }
     else
     {
-        if (colorflag       == TRUE)
-        {
-            fprintf (stdout, "\e[1;36m");
-        }
         switch (fmt)
         {
             case FORMAT_HEX:
                 if (flag    == TRUE)
                 {
-                    fprintf (stdout, "0x%.8X",
-                            IMEMGET(IMEMGET(value)));
+                    fprintf (stdout, "0x%.8X", IMEMGET(IMEMGET(value)));
                 }
                 else
                 {
-                    fprintf (stdout, "0x%.8X",
-                            IMEMGET(value));
+                    fprintf (stdout, "0x%.8X", IMEMGET(value));
                 }
                 break;
 
             case FORMAT_LOWERHEX:
                 if (flag    == TRUE)
                 {
-                    fprintf (stdout, "0x%.8x",
-                            IMEMGET(IMEMGET(value)));
+                    fprintf (stdout, "0x%.8x", IMEMGET(IMEMGET(value)));
                 }
                 else
                 {
-                    fprintf (stdout, "0x%.8x",
-                            IMEMGET(value));
+                    fprintf (stdout, "0x%.8x", IMEMGET(value));
                 }
                 break;
 
             case FORMAT_UNSIGNED:
                 if (flag    == TRUE)
                 {
-                    fprintf (stdout, "%u",
-                            IMEMGET(IMEMGET(value)));
+                    fprintf (stdout, "%u",     IMEMGET(IMEMGET(value)));
                 }
                 else
                 {
-                    fprintf (stdout, "%u",
-                            IMEMGET(value));
+                    fprintf (stdout, "%u",     IMEMGET(value));
                 }
                 break;
 
             case FORMAT_OCTAL:
                 if (flag    == TRUE)
                 {
-                    fprintf (stdout, "0%o",
-                            IMEMGET(IMEMGET(value)));
+                    fprintf (stdout, "0%o",    IMEMGET(IMEMGET(value)));
                 }
                 else
                 {
-                    fprintf (stdout, "0%o",
-                            IMEMGET(value));
+                    fprintf (stdout, "0%o",    IMEMGET(value));
                 }
                 break;
 
             case FORMAT_FLOAT:
                 if (flag    == TRUE)
                 {
-                    fprintf (stdout, "%.2f",
-                            FMEMGET(IMEMGET(value)));
+                    fprintf (stdout, "%.2f",   FMEMGET(IMEMGET(value)));
                 }
                 else
                 {
-                    fprintf (stdout, "%.2f",
-                            FMEMGET(value));
+                    fprintf (stdout, "%.2f",   FMEMGET(value));
                 }
                 break;
 
             case FORMAT_SIGNED:
                 if (flag    == TRUE)
                 {
-                    fprintf (stdout, "%d",
-                            IMEMGET(IMEMGET(value)));
+                    fprintf (stdout, "%d",     IMEMGET(IMEMGET(value)));
                 }
                 else
                 {
-                    fprintf (stdout, "%d",
-                            IMEMGET(value));
+                    fprintf (stdout, "%d",     IMEMGET(value));
                 }
                 break;
 
@@ -457,10 +274,6 @@ void  output_mem (uint32_t  value, uint8_t  fmt,  uint8_t  flag, uint8_t *label)
                     data     = data / 2;
                 }
                 break;
-        }
-        if (colorflag       == TRUE)
-        {
-            fprintf (stdout, "\e[m");
         }
     }
 
