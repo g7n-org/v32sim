7879a4bbfc1eee2e85ee9015da6ca4577eac7384
Matthew Haas <wedge@lab46.g7n.org>
2026-07-31 12:19:39 -0400

    resolving get_line() compile warning

--- main.c
+++ main.c
@@ -114,7 +114,7 @@ int32_t   main (int32_t  argc, char **argv)
     uint32_t  value                 = 0;
     uint32_t  line_number           = 0;
     uint32_t  old_count             = 0;
-    uint8_t  *line_input            = NULL;
+    char     *line_input            = NULL;
     int32_t   index                 = 0;
     size_t    buffer_size           = 0;
 
